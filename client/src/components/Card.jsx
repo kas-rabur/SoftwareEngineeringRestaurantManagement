a49f9762b0523487ca18248b5422e86aa5c09f7a
kas-rabur <raburskikacper@gmail.com>
2025-04-16 10:50:23 +0100

    added basic unfinished home page as well as improved / changed my card component to make it more simple

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,18 +1,12 @@
 import React from "react";
 import "../styles/Card.css";
 
-const Card = ({ id, title, description, expandedContent, isExpanded, onToggle }) => (
-  <div className="card" id={id}>
-    <div className="card-header" onClick={() => onToggle(id)}>
+const Card = ({ title, description }) => (
+  <div className="card">
+    <div className="card-header">
       <h2>{title}</h2>
-      <span className={`arrow ${isExpanded ? "expanded" : ""}`}>▼</span>
     </div>
     <p>{description}</p>
-    {isExpanded && (
-      <div className="card-expanded">
-        {expandedContent || <p>No additional information available.</p>}
-      </div>
-    )}
   </div>
 );
 
--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "../styles/HomePage.css"; 
-
+import Card from "./Card";
+import "../styles/Card.css";
+import restaurantImage from "../images/restaurant_stock.webp"; 
 
 const HomePage = () => {
     return (
-        <div>hello home</div>
+        <div className="home-container">
+            <Card title={"Welcome to Our Restaurant!"} description={"this is some filler inforamtion about the website to test the layout and more."}></Card>
+            <Card title={"This is where an image will go!"} description={"this is some more filler to see"}></Card>
+        
+        </div>
     )
 }
 
